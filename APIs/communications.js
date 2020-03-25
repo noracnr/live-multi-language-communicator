6a0b4868de8a5a67a531c62c32cd28ab86b6a3c2
Ningrong Chen <823411732@qq.com>
2020-03-25 17:33:54 -0400

    Update communications.js

--- APIs/communications.js
+++ APIs/communications.js
@@ -1,36 +1,7 @@
+// import my own modules
 var connection = require('./connection.js');
-const translate = require('google-translate-api'); // https://www.npmjs.com/package/google-translate-api
-// speech to text: https://www.youtube.com/watch?v=naZ8oEKuR44
-// install node.js client 
-// npm install --save @google-cloud/speech
-const speech = require('@google-cloud/speech');
+var speech2text = require('./speech2text.js');
+var text2text = require('./text2text.js');
+var text2speech = require('./text2speech.js');
 // file system
 const fs = require('fs');
-
-
-async function main() {
-
-	// Speech2text
-	const client = new speech.SpeechClient();
-	const filename = 'the path of audio.raw';
-
-	const file = fs.readFileSync(filename);
-	const audioBytes = file.toString('base64');
-
-	const audio = {
-		content : audioBytes
-	};
-
-	const config = {
-		encoding: 'LINEAR16',
-		sampleRateHertz: 16000,
-		languageCode: 'en-US'
-	};
-
-	const request = {
-		audio: audio,
-		config: config
-	};
-
-	const [response] = await client.recognize(request);
-}
