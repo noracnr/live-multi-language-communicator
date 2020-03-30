1cc6b060c87d5716c6123a22b36794606b06ea3d
GreenyWind <whelzyq@163.com>
2020-03-30 17:45:22 -0400

    Delete communications.js

--- APIs/communications.js
+++
@@ -1,7 +0,0 @@
-// import my own modules
-var connection = require('./connection.js');
-var speech2text = require('./speech2text.js');
-var text2text = require('./text2text.js');
-var text2speech = require('./text2speech.js');
-// file system
-const fs = require('fs');
