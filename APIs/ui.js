726c56187ccd0932791e198c83cc8ecc73f53747
GreenyWind <whelzyq@163.com>
2020-03-30 17:46:12 -0400

    Delete ui.js

--- APIs/ui.js
+++
@@ -1,8 +0,0 @@
-// user interface.
-var connection = require('./connection.js');
-var communication = require('./communication.js');
-
-
-// Get media devices like microphone, camera, using WebRTC
-
-// Express for UI
